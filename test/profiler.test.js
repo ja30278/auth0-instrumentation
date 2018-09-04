55486abf1d9b744606ee020e697feee7ca934965
Jon Allie <jon.allie@auth0.com>
2018-09-04 13:19:25 -0400

    Improve profiler tests (which are currently flaky in the CI)
    - Correctly catch 'assert' exceptions, and propagate to done
    - Use Sinon fakes to assert that 'report' is called, rather than logInfo
    - Use memorystream + a fake snapshot rather than invoke the snapshotter
    and writing to files during the test, which can

--- test/profiler.test.js
+++ test/profiler.test.js
@@ -1,7 +1,9 @@
-var assert = require('assert');
-var sinon = require('sinon');
+const assert = require('assert');
+const sinon = require('sinon');
+const MemoryStream = require('memorystream');
+const v8Profiler = require('v8-profiler-node8');
 
-var Profiler = require('../lib/profiler');
+const Profiler = require('../lib/profiler');
 
 describe('Profiler', function() {
   var profiler, agent;
@@ -16,20 +18,31 @@ describe('Profiler', function() {
     };
     profiler = new Profiler(agent, { name: 'test' }, { HUNT_MEMORY_LEAKS: true });
     // avoid having to create workers
-    process.send = function() {};
+    sinon.replace(process, 'send', sinon.fake());
   });
 
   afterEach(function() {
-    process.send = undefined;
+    sinon.restore();
   });
 
   describe('#createThrottledSnapshot', function() {
-    it('should create a snapshot and log', function(done) {
+    it('should create a snapshot and report', function(done) {
+      const snapshot = {
+        'export': sinon.fake.returns(new MemoryStream(['snapshot']))
+      };
+      sinon.replace(profiler, 'report', sinon.fake());
+      sinon.replace(v8Profiler, 'takeSnapshot', sinon.fake.returns(snapshot)); 
       this.timeout(3000);
       profiler.createThrottledSnapshot('testing');
       setTimeout(() => {
-        assert(agent.logger.info.calledOnce);
-        done();
+        try {
+          assert(1, v8Profiler.takeSnapshot.callCount);
+          assert(1, profiler.report.callCount);
+          assert(1, snapshot.export.callCount);
+          done();
+        } catch (err) {
+          done(err);
+        }
       }, 1000);
     });
   });
@@ -38,9 +51,13 @@ describe('Profiler', function() {
     it('should create a profile', function(done) {
       this.timeout(3000);
       profiler.createProfile(1000, function(err, path) {
-        assert.ifError(err);
-        assert(path);
-        done();
+        try {
+          assert.ifError(err);
+          assert(path);
+          done();
+        } catch (err) {
+          done(err);
+        }
       });
     });
   });
